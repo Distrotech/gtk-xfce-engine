569c250d5c84349e65fe90121ee2d28bdee8378e
Olivier Fourdan <fourdan.olivier@wanadoo.fr>
2002-12-10 22:42:36 +0000

    Change various frame draw styles in smooth mode.
    (Old svn revision: 146)

--- xfce_theme_draw.c
+++ xfce_theme_draw.c
@@ -348,40 +348,22 @@ static void draw_shadow(GtkStyle * style, GdkWindow * window, GtkStateType state
         case GTK_SHADOW_IN:
             if(XFCE_RC_STYLE(style->rc_style)->smooth_edge)
             {
-                if(DETAIL("hscrollbar") || DETAIL("vscrollbar") || DETAIL("trough"))
-                {
-                    gdk_draw_line(window, style->dark_gc[state_type], x, y, x + width - 2, y);
-                    gdk_draw_line(window, style->dark_gc[state_type], x, y, x, y + height - 2);
+                gdk_draw_line(window, style->dark_gc[state_type], x + 1, y, x + width - 2, y);
+                gdk_draw_line(window, style->dark_gc[state_type], x, y + 1, x, y + height - 2);
 
-                    gdk_draw_line(window, style->dark_gc[state_type], x, y + height - 1, x + width - 1, y + height - 1);
-                    gdk_draw_line(window, style->dark_gc[state_type], x + width - 1, y, x + width - 1, y + height - 1);
+                gdk_draw_line(window, style->dark_gc[state_type], x + 1, y + height - 1, x + width - 2, y + height - 1);
+                gdk_draw_line(window, style->dark_gc[state_type], x + width - 1, y + 1, x + width - 1, y + height - 2);
 
-                    gdk_draw_line(window, style->bg_gc[state_type], x + 1, y + 1, x + width - 2, y + 1);
-                    gdk_draw_line(window, style->bg_gc[state_type], x + 1, y + 1, x + 1, y + height - 2);
+                gdk_draw_line(window, style->mid_gc[state_type], x + 1, y + 1, x + width - 2, y + 1);
+                gdk_draw_line(window, style->mid_gc[state_type], x + 1, y + 1, x + 1, y + height - 2);
 
-                    gdk_draw_line(window, style->bg_gc[state_type], x + 2, y + height - 2, x + width - 2, y + height - 2);
-                    gdk_draw_line(window, style->bg_gc[state_type], x + width - 2, y + 2, x + width - 2, y + height - 2);
+                gdk_draw_line(window, style->light_gc[state_type], x + 2, y + height - 2, x + width - 2, y + height - 2);
+                gdk_draw_line(window, style->light_gc[state_type], x + width - 2, y + 2, x + width - 2, y + height - 2);
 
-                }
-		else
-		{
-                    gdk_draw_line(window, style->dark_gc[state_type], x + 1, y, x + width - 2, y);
-                    gdk_draw_line(window, style->dark_gc[state_type], x, y + 1, x, y + height - 2);
-
-                    gdk_draw_line(window, style->dark_gc[state_type], x + 1, y + height - 1, x + width - 2, y + height - 1);
-                    gdk_draw_line(window, style->dark_gc[state_type], x + width - 1, y + 1, x + width - 1, y + height - 2);
-
-                    gdk_draw_line(window, style->bg_gc[state_type], x + 1, y + 1, x + width - 2, y + 1);
-                    gdk_draw_line(window, style->bg_gc[state_type], x + 1, y + 1, x + 1, y + height - 2);
-
-                    gdk_draw_line(window, style->bg_gc[state_type], x + 2, y + height - 2, x + width - 2, y + height - 2);
-                    gdk_draw_line(window, style->bg_gc[state_type], x + width - 2, y + 2, x + width - 2, y + height - 2);
-
-                    gdk_draw_point(window, style->mid_gc[state_type], x, y);
-                    gdk_draw_point(window, style->mid_gc[state_type], x + width - 1, y);
-                    gdk_draw_point(window, style->mid_gc[state_type], x, y + height - 1);
-                    gdk_draw_point(window, style->mid_gc[state_type], x + width - 1, y + height - 1);
-                }
+                gdk_draw_point(window, style->mid_gc[state_type], x, y);
+                gdk_draw_point(window, style->mid_gc[state_type], x + width - 1, y);
+                gdk_draw_point(window, style->mid_gc[state_type], x, y + height - 1);
+                gdk_draw_point(window, style->mid_gc[state_type], x + width - 1, y + height - 1);
 	    }
             else
             {
@@ -415,7 +397,7 @@ static void draw_shadow(GtkStyle * style, GdkWindow * window, GtkStateType state
                     gdk_draw_line(window, style->mid_gc[state_type], x + 1, y + height - 2, x + width - 2, y + height - 2);
                     gdk_draw_line(window, style->mid_gc[state_type], x + width - 2, y + 1, x + width - 2, y + height - 2);
                 }
-		else if(DETAIL("menubar") || DETAIL("toolbar") || DETAIL("dockitem") || DETAIL("dockitem_bin"))
+		else if(DETAIL("menubar") || DETAIL("toolbar") || DETAIL("dockitem") || DETAIL("dockitem_bin") || DETAIL("frame") || DETAIL("menu"))
                 {
                     gdk_draw_line(window, style->light_gc[state_type], x, y, x + width - 2, y);
                     gdk_draw_line(window, style->light_gc[state_type], x, y, x, y + height - 2);
@@ -428,20 +410,6 @@ static void draw_shadow(GtkStyle * style, GdkWindow * window, GtkStateType state
 
                     gdk_draw_line(window, style->bg_gc[state_type], x + 2, y + height - 2, x + width - 2, y + height - 2);
                     gdk_draw_line(window, style->bg_gc[state_type], x + width - 2, y + 2, x + width - 2, y + height - 2);
-                }
-                else if(DETAIL("frame") || DETAIL("menu"))
-                {
-                    gdk_draw_line(window, style->dark_gc[state_type], x, y, x + width - 1, y);
-                    gdk_draw_line(window, style->dark_gc[state_type], x, y, x, y + height - 1);
-
-                    gdk_draw_line(window, style->dark_gc[state_type], x + 1, y + height - 1, x + width - 1, y + height - 1);
-                    gdk_draw_line(window, style->dark_gc[state_type], x + width - 1, y + 1, x + width - 1, y + height - 1);
-
-                    gdk_draw_line(window, style->light_gc[state_type], x + 1, y + 1, x + width - 3, y + 1);
-                    gdk_draw_line(window, style->light_gc[state_type], x + 1, y + 1, x + 1, y + height - 3);
-
-                    gdk_draw_line(window, style->mid_gc[state_type], x + 1, y + height - 2, x + width - 2, y + height - 2);
-                    gdk_draw_line(window, style->mid_gc[state_type], x + width - 2, y + 1, x + width - 2, y + height - 2);
                 }
                 else
                 {
