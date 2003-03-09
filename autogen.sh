ccc1550d762dc48495f8dc3ac1cb96b255cc6706
Jasper Huijsmans <jbhuijsmans@home.nl>
2003-03-09 19:14:00 +0000

    Fix autogen.sh to run libtoolize
    (Old svn revision: 166)

--- autogen.sh
+++ autogen.sh
@@ -40,7 +40,7 @@ DIE=0
   }
 }
 
-(grep "^AM_PROG_LIBTOOL" $srcdir/configure.in >/dev/null) && {
+(grep "^AC_PROG_LIBTOOL" $srcdir/configure.in >/dev/null) && {
   (libtool --version) < /dev/null > /dev/null 2>&1 || {
     echo
     echo "**Error**: You must have \`libtool' installed."
@@ -120,7 +120,7 @@ do
         echo "Running xml-i18n-toolize..."
 	xml-i18n-toolize --copy --force --automake
       fi
-      if grep "^AM_PROG_LIBTOOL" configure.in >/dev/null; then
+      if grep "^AC_PROG_LIBTOOL" configure.in >/dev/null; then
 	if test -z "$NO_LIBTOOLIZE" ; then 
 	  echo "Running libtoolize..."
 	  libtoolize --force --copy
